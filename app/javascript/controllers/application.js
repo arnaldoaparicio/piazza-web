bd4fdb36d1976fee02cb51b8765228b3c630db57
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-07-28 14:46:52 -0400

    Add addendum to register 'toggle' controller

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,6 +2,8 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
+application.register("toggle", ToggleController)
+
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
