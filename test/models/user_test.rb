793b62fd0a41810a38cf8c819b9250bee27b7e82
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-07-28 15:12:33 -0400

    Add tests to check for valid user credentials

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,31 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "requires a name" do
+    @user = User.new(name: "", email: "johndoe@example.com")
+    assert_not @user.valid?
+
+    @user.name = "John"
+    assert @user.valid?
+  end
+
+  test "requires a valid email" do
+    @user = User.new("John", email: "")
+    assert_not @user.valid?
+
+    @user.email = "invalid"
+    assert_not @user.valid?
+
+    @user.email = "johnoe@example.com"
+    assert @user.valid
+  end
+
+  test "requires a unique email" do
+    @existing_user = User.create(name: "John", email: "jd@example.com")
+    assert @existing_user.persisted?
+
+    @user = User.new(name: "Jon", email: "jd@example.com")
+    assert_not @user.valid?
+  end
 end
