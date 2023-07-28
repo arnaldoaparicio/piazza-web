625f399d528f7c4b1292d529310728e4472f3918
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-07-28 17:52:34 -0400

    Add routes to sign up and show the feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'feed/show'
-  get 'users/new'
-  get 'users/create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "feed#show"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "sign_up", to: "users#new"
+  post "sign_up", to: "users#create"
 end
