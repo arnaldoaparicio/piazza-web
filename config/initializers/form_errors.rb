120dad91fc996e318ab0c5bf68f30e0ea8cf3cc3
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-07-30 11:06:13 -0400

    Adjust spacing

--- config/initializers/form_errors.rb
+++ config/initializers/form_errors.rb
@@ -4,9 +4,9 @@ ActionView::Base.field_error_proc = -> (html_tag, instance) {
         html.children.add_class("is-danger")
 
         error_message_markup = <<~HTML
-        <p class='help is-danger'>
-            #{sanitize(instance.error_message.to_sentence)}
-        </p>
+            <p class='help is-danger'>
+                #{sanitize(instance.error_message.to_sentence)}
+            </p>
         HTML
 
         "#{html.to_s}#{error_message_markup}".html_safe
