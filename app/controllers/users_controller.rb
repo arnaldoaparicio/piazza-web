de0508d3332912e9483ece86fc5c110b3a75ccb4
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-07-30 20:06:40 -0400

    Adjust formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,9 @@ class UsersController < ApplicationController
     end
   end
 
-
   private
-    def user_params
-      params.require(:user).permit(:name, :email, :password)
-    end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,22 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email,
-        format: { with: URI::MailTo::EMAIL_REGEXP },
-        uniqueness: { case_sensitive: false }
+  validates :name, presence: true
+  validates :email,
+            format: { with: URI::MailTo::EMAIL_REGEXP },
+            uniqueness: { case_sensitive: false }
 
-    has_many :memberships, dependent: :destroy
-    has_many :organizations, through: :memberships
+  has_many :memberships, dependent: :destroy
+  has_many :organizations, through: :memberships
 
-    # callback
-    before_validation :strip_extraneous_spaces
+  # callback
+  before_validation :strip_extraneous_spaces
 
-    has_secure_password
-    validates :password, presence: true, length: { minimum: 8 }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }
 
-    private
+  private
 
-        def strip_extraneous_spaces
-            self.name = self.name&.strip
-            self.email = self.email&.strip
-        end
+  def strip_extraneous_spaces
+    self.name = name&.strip
+    self.email = email&.strip
+  end
 end
